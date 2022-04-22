8c26db368f6d9a4b1affd8a1f08710d267b8a2a0
Craig <me@craigwilson.blog>
2022-04-23 07:40:02 +1000

    Update LCD.sh

--- KALI-Config/LCD.sh
+++ KALI-Config/LCD.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sudo rm -rf LCD-show 
-git clonehttps://github.com/CraigWilsonOZ/LCD-show.git
+git clone https://github.com/CraigWilsonOZ/LCD-show.git
 chmod -R 755 LCD-show 
 cd LCD-show/
 sudo ./MHS35-show
